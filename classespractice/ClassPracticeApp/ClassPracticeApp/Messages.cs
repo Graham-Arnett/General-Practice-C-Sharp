97cec7bacb8c87c5cee89fffbbef2981a2759cee
Graham Arnett <GrahamKentArnett@outlook.com>
2025-06-03 13:18:57 -0400

    init commit

---
+++ classespractice/ClassPracticeApp/ClassPracticeApp/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPracticeApp
+{
+    static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to my program.");
+        }
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for something.");
+        }
+        public static void Bye()
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ classespractice/ClassPracticeApp/ClassPracticeApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ClassPracticeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //class = bundle of related code
+            //can be used as a blueprint to create objects
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+            
+            Console.ReadKey();
+
+        }
+    }
+   
+     
+    
+}
