d9b08b108f60acc9ed3888af4dd1d606aca08998
Graham Arnett <GrahamKentArnett@outlook.com>
2025-06-07 17:02:44 -0400

    still working, will be for a few days probably

--- classespractice/ttrpgRollerApp/ttrpgRollerApp/Program.cs
+++ classespractice/ttrpgRollerApp/ttrpgRollerApp/Program.cs
@@ -5,16 +5,33 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Random Random = new Random();
+            Roller.Stats();
+            Roller.Classes();
+            Roller.Backstory();
+            Roller.Races();
         }
     }
     public class Roller
     {
-        Random Random = new Random();
-        public static void Stats()
+        //Random Random = new Random();
+        public static void Stats(Random random)
         {
+            //int score = random.Next(1,7);
+            int[] scoreArray = [6];
 
+            for(int i = 0; i <1; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    int roll = random.Next(1,7);
+                    scoreArray[i] += roll;
+
+                }
+            }
+            Console.WriteLine($"Your strength is: {scoreArray}");
         }
-        public static void Classes()
+        public static void Classes(Random random)
         {
 
         }
