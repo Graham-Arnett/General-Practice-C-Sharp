3f2e2d35872d992a77b6ba1f712033e89839f1a5
Graham Arnett <GrahamKentArnett@outlook.com>
2025-07-14 18:44:40 -0400

    init commit 4 func, literally just started

---
+++ fourfunccalc/fourfunccalc/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace fourfunccalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //I was gonna do a ton on this but I have a super headache so not rn
+        }
+    }
+}
