b21bd8f96cf573a20b216337aae293de7b6e587b
Graham Arnett <GrahamKentArnett@outlook.com>
2025-07-17 13:34:24 -0400

    the midpoint calculator is done and working

---
+++ midPointApp/midPointApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace midPointApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Midpoint Calculator app, again");
+            char redo;
+            double[] n = new double[4];
+            do
+            {
+                for(int i = 0;i < 4; i++)
+                {
+                    Console.Write("\nEnter a number: ");
+                    n[i] = double.Parse(Console.ReadLine());
+                }
+                double xpoint = (n[0] + n[1])/2;
+                double ypoint = (n[2] - n[3])/2;
+                Console.WriteLine($"Your midpoint is ({xpoint} , {ypoint})");
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
