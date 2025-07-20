05aa02b25db5a47146c6216ccb86c935c915201c
Graham Arnett <GrahamKentArnett@outlook.com>
2025-07-19 22:51:33 -0400

    was a bit busy today, wanted to do some practice to remember how modulos work

---
+++ EvenCheckerApp/EvenCheckerApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EvenCheckerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even number checker!");
+            char redo;
+            do
+            {
+                Console.Write("\n\nEnter a number: ");
+                int entry = int.Parse(Console.ReadLine());
+                if (entry % 2 == 0)
+                {
+                    Console.WriteLine("That is even!");
+                }
+                else
+                {
+                    Console.WriteLine("That is odd!");
+                }
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
