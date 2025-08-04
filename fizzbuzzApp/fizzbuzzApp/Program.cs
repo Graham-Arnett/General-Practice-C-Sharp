f563e248ff9c74507738af2b014a079c01005b34
Graham Arnett <GrahamKentArnett@outlook.com>
2025-08-03 21:06:53 -0400

    I know its still lacking, but I've been sick for the last 4 days

--- fizzbuzzApp/fizzbuzzApp/Program.cs
+++ fizzbuzzApp/fizzbuzzApp/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("FizzBuzz");
+            char redo;
+            do
+            {
+                Console.Write("Please enter a number: ");
+                int num = int.Parse(Console.ReadLine());
+                if(num % 3 == 0)
+                {
+                    Console.WriteLine("FIZZ");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("BUZZ");
+                }
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            }while(redo == 'y'||redo=='Y');
         }
     }
 }
