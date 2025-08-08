6ac120bc737a39962ad0a66ceaad18ba8285b037
Graham Arnett <GrahamKentArnett@outlook.com>
2025-08-07 20:45:28 -0400

    I am incredibly sick and have been all week, I just wanted to get back in the habit of making my github green

--- fizzbuzzApp/fizzbuzzApp/Program.cs
+++ fizzbuzzApp/fizzbuzzApp/Program.cs
@@ -8,20 +8,24 @@
             char redo;
             do
             {
-                Console.Write("Please enter a number: ");
+                Console.Write("\n\nPlease enter a number: ");
                 int num = int.Parse(Console.ReadLine());
                 if(num % 5 == 0 && num % 3 == 0)
                 {
-                    Console.WriteLine("FIZBUZZ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("FIZZBUZZ");
                 }
                 else if(num % 3 == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("FIZZ");
                 }
                 else if (num % 5 == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("BUZZ");
                 }
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
             }while(redo == 'y'||redo=='Y');
