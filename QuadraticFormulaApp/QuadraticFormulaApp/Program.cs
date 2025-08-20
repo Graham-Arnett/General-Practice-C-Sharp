319ec8b883e9b6c69e57682c6ea1d1abce1db03c
Graham Arnett <GrahamKentArnett@outlook.com>
2025-08-20 19:16:24 -0400

    was tired and busy today

---
+++ QuadraticFormulaApp/QuadraticFormulaApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace QuadraticFormulaApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the quadratic formula app");
+            char redo;
+            do
+            {
+                Console.Write("\n\nWould you like to go again?(y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'Y' || redo == 'y');
+        }
+    }
+}
