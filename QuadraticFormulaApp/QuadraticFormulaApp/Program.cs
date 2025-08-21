b0d74778257cc3fb3d8ab249323b811cf583c21e
Graham Arnett <GrahamKentArnett@outlook.com>
2025-08-21 19:11:59 -0400

    still going

--- QuadraticFormulaApp/QuadraticFormulaApp/Program.cs
+++ QuadraticFormulaApp/QuadraticFormulaApp/Program.cs
@@ -6,8 +6,16 @@
         {
             Console.WriteLine("Welcome to the quadratic formula app");
             char redo;
+            double[] n = new double[3];
+            string[] order = {"FIRST","SECOND","THIRD"};
             do
             {
+                for (int i = 0;i < 0; i++)
+                {
+                    Console.WriteLine($"\n\nPlease enter your {order[i]} number: ");
+                    n[i] = double.Parse(Console.ReadLine());
+                }
+
                 Console.Write("\n\nWould you like to go again?(y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'Y' || redo == 'y');
