130fb635ec4036f13acf423eaad5a8884b37a412
Graham Arnett <GrahamKentArnett@outlook.com>
2025-08-22 18:24:51 -0400

    not sure what to do, getting NaN

--- QuadraticFormulaApp/QuadraticFormulaApp/Program.cs
+++ QuadraticFormulaApp/QuadraticFormulaApp/Program.cs
@@ -10,12 +10,15 @@
             string[] order = {"FIRST","SECOND","THIRD"};
             do
             {
-                for (int i = 0;i < 0; i++)
+                for (int i = 0;i < 3; i++)
                 {
-                    Console.WriteLine($"\n\nPlease enter your {order[i]} number: ");
+                    Console.Write($"\n\nPlease enter your {order[i]} number: ");
                     n[i] = double.Parse(Console.ReadLine());
                 }
-
+                double squaredPart = Math.Sqrt(((n[1] * n[1]) - 4*(n[0] * n[2])));
+                double posVer = (-n[1] + squaredPart) / (2 * n[0]);
+                double negVer = (n[1] - squaredPart) / (2 * n[0]);
+                Console.WriteLine($"The solutions are: {posVer} , {negVer}");
                 Console.Write("\n\nWould you like to go again?(y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'Y' || redo == 'y');
