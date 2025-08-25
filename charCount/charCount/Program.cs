fe25fd2dffd897385c6a7df516d408c41d17bf67
Graham Arnett <GrahamKentArnett@outlook.com>
2025-08-25 13:57:54 -0400

    was nauseous today, I know its been happening a ton so I wanted to do a simple console app for counting characters in a string

---
+++ charCount/charCount/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace charCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Character Counting App");
+            char redo;
+            do
+            {
+                int charCount = 0;
+                Console.Write("\n\nWould you please enter some text: ");
+                string word = Console.ReadLine();
+                for(int i = 0; i < word.Length; i++)
+                {
+                    charCount++;
+                }
+                Console.WriteLine($"Your character count is: {charCount} characters!");
+                Console.Write("\n\n\nWould you like to go again? (y/n:) ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'Y' || redo == 'y');
+        }
+    }
+}
