ec03b8d32f995cee8df9e903e376bc89237542cc
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-08 14:15:52 -0400

    added stuff for numbers, special characters and spaces because why not

--- MoreVowelorCons/MoreVowelorCons/Program.cs
+++ MoreVowelorCons/MoreVowelorCons/Program.cs
@@ -9,10 +9,16 @@
             char redo;
             char[] c = {'q','w','r','t','y','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
             char[] v = {'a','e','i','o','u'};
+            char[] n = {'1','2','3','4','5','6','7','8','9','0'};
+            char[] s = {'!','@','#','$','%','^','&','*','(',')','-','_','~','`','=','+','{','[',']','}'
+            ,'/','|',':',';','"','<',',','.','>','?'};
             do
             {
                 int vowelCount = 0;
                 int consCount = 0;
+                int numCount = 0;
+                int specCount = 0;
+                int spaceCount = 0;
                 Console.Write("\n\nEnter some text: ");
                 string text = Console.ReadLine();
                 for (int i = text.Length -1; i >= 0; i--)
@@ -24,6 +30,18 @@
                     else if (v.Contains(text[i]))
                     {
                         vowelCount++;
+                    }
+                    else if (n.Contains(text[i]))
+                    {
+                        numCount++;
+                    }
+                    else if (s.Contains(text[i]))
+                    {
+                        specCount++;
+                    }
+                    else if (text[i] == ' ')
+                    {
+                        spaceCount++;
                     }
                 }
                 if(vowelCount > consCount)
@@ -38,7 +56,8 @@
                 {
                     Console.WriteLine("You had more consonants.\n");
                 }
-                Console.WriteLine($"You had {consCount} consonants and {vowelCount} vowels!");
+                Console.WriteLine($"You had {consCount} consonants and {vowelCount} vowels!" +
+                    $"\nMuch less importantly, you had {numCount} numbers, {specCount} special characters and {spaceCount} spaces.");
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'y' || redo == 'Y');
