7ba6dde87aa0ce82004951f9f6d4f32ebe6b046a
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-10 01:06:23 -0400

    very simple coin flip. Ive made them before but I just made this for some silly game and I needed a quick coinflip for it

---
+++ Coinflip/Coinflip/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Coinflip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coinflip app");
+            Random random = new Random();
+            char redo;
+            do
+            { 
+                int coin = random.Next(1,3);
+                if(coin == 1)
+                {
+                    Console.WriteLine($"\nYour result is: HEADS");
+                }
+                else if(coin == 2)
+                {
+                    Console.WriteLine("\nYour result is: TAILS");
+                }
+                //Console.WriteLine($"Your result is: ");
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
