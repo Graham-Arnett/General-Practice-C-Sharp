5aa11aa496b121541df66a39aa6409452b9df54a
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-09 20:43:48 -0400

    init commit generic practice dice game

---
+++ DiceGame/DiceGame/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dice Game app");
+            char redo;
+            do
+            {
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
