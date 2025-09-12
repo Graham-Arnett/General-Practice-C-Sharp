a02325041f7320454df98d2b3a0c52081f45f5c3
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-11 22:30:09 -0400

    still going, got busy on job apps today

--- DiceGame/DiceGame/Program.cs
+++ DiceGame/DiceGame/Program.cs
@@ -5,9 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Dice Game app");
+            Random random = new Random();
             char redo;
             do
             {
+                int roll = random.Next(1, 7);
+                int[] r = new int[2];
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'y' || redo == 'Y');
