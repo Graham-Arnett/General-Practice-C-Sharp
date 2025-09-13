2421a5d5ac8a3ea8007d2ba8010a038c4f6c5a59
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-12 20:30:51 -0400

    forget how to deal with randoms so I'll tackle this tomorrow

--- DiceGame/DiceGame/Program.cs
+++ DiceGame/DiceGame/Program.cs
@@ -7,10 +7,21 @@
             Console.WriteLine("Dice Game app");
             Random random = new Random();
             char redo;
+            int roll;
             do
             {
-                int roll = random.Next(1, 7);
+                
                 int[] r = new int[2];
+                for(int i = 0; i < 2; i++)
+                {
+                    roll = random.Next(1, 7);
+                    roll = r[i];
+                }
+                Console.WriteLine($"You got {r[0]} for your first roll and {r[1]} for your second roll!");
+                if (r[0] == r[1])
+                {
+                    Console.WriteLine("Ewwww you got doubles that's GROSS");
+                }
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'y' || redo == 'Y');
