7f1c68cd169f91697f942bc6b308b6141c02b55e
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-20 17:49:52 -0400

    redoing the traveltime app because I didn't have an idea for today

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Travel Time App!");
+            char redo;
+            do
+            {
+                Console.Write("Please enter a distance here in miles: ");
+                double miles = double.Parse(Console.ReadLine());
+                Console.Write("");
+                Console.Write("\n\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
