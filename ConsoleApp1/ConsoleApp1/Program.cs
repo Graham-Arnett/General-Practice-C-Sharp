2d53df0671fb9a7defe32c10a557c87121a428a8
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-21 20:29:50 -0400

    still going

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,10 @@
             {
                 Console.Write("Please enter a distance here in miles: ");
                 double miles = double.Parse(Console.ReadLine());
-                Console.Write("");
+                Console.Write("Enter a speed in miles per hour: ");
+                double speed = double.Parse(Console.ReadLine());
+
+                double hour;
                 Console.Write("\n\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'y' || redo == 'Y');
