f77f600b60ac0de48939771a97a124a8b082bad3
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-29 14:55:31 -0400

    init commit triangle draw

---
+++ TriangleDraw/TriangleDraw/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TriangleDraw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle Drawing App");
+            char redo;
+
+            do
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
