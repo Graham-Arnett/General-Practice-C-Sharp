86ce0d55626d24aecdc311ef99807659a0f067eb
Graham Arnett <GrahamKentArnett@outlook.com>
2025-10-04 23:15:44 -0400

    init commit drawAFace

---
+++ drawAFace/drawAFace/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace drawAFace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Facedraw app! This will use user input fromp prompts to combine strings to make an ascii face");
+        }
+    }
+}
