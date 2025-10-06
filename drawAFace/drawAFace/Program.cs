b5b39eb303bc2886b73517c644195d3cc0031e40
Graham Arnett <GrahamKentArnett@outlook.com>
2025-10-05 20:05:29 -0400

    still working

--- drawAFace/drawAFace/Program.cs
+++ drawAFace/drawAFace/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Facedraw app! This will use user input fromp prompts to combine strings to make an ascii face");
+            char redo;
+            do
+            {
+                Console.Write("Would you like to design your face or let it randomize? (d or r): ");
+                char initChoice = Console.ReadKey().KeyChar;
+                if(initChoice == 'r')
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        //will roll 3 times for the top, middle, and bottom of the face)
+                    }
+                }
+                Console.Write("\n\nWould you like to continue? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
         }
     }
 }
